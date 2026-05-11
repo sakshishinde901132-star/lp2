59808e48570027fae6262af26cf7a6064245890f
sakshishinde901132-star <sakshishinde901132@gmail.com>
2026-05-11 21:39:06 +0530

    Update t1.py

--- t1.py
+++ t1.py
@@ -1,2 +1,19 @@
-print("hello")
-print("te")
+
+
+num1 = float(input("Enter first number: "))
+op = input("Enter operator (+, -, *, /): ")
+num2 = float(input("Enter second number: "))
+
+if op == "+":
+    print("Result:", num1 + num2)
+elif op == "-":
+    print("Result:", num1 - num2)
+elif op == "*":
+    print("Result:", num1 * num2)
+elif op == "/":
+    if num2 != 0:
+        print("Result:", num1 / num2)
+    else:
+        print("Error: Cannot divide by zero")
+else:
+    print("Invalid operator")
